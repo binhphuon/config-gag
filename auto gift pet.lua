295804dcd74b63787509c933fbc2c907af8dd60a
binhphuon <131878934+binhphuon@users.noreply.github.com>
2025-09-08 23:56:21 +0700

    1

--- auto gift pet.lua
+++ auto gift pet.lua
@@ -166,7 +166,7 @@ end
 
 -- Vòng lặp chính
 while true do
-    task.wait(30)
+    task.wait(1)
     for _, cfg in ipairs(DataGetTool) do
         for _, p in ipairs(Players:GetPlayers()) do
             if table.find(cfg.playerlist, p.Name) then
