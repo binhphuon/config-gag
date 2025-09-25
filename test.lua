a0d630e0f4733904f0e6b32ac30edf7ddfe51f66
binhphuon <131878934+binhphuon@users.noreply.github.com>
2025-09-26 01:19:28 +0700

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,8 @@
+    {
+        name_pet    = "Dragonfly",   --
+        min_age     = 1,
+        max_age     = 101,
+        min_weight  = 2.5,          
+        unequip_Pet = true,
+        playerlist  = {"DzHDV6hbZk","jbVQQmEb0P"},
+    },
