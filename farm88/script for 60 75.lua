c0336ad806a04d87e1ba5ebec900473510a894d2
binhphuon <131878934+binhphuon@users.noreply.github.com>
2025-11-02 16:41:55 +0700

    Create script for 60 75.lua

---
+++ farm88/script for 60 75.lua
@@ -0,0 +1,6 @@
+
+task.spawn(function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/zcreatefilecc88.lua"))()
+end)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/zgiftfeed88.lua"))()
