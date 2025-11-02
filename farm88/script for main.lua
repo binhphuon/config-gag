869213dd6cb3849057360bb538b01dc532f8bf45
binhphuon <131878934+binhphuon@users.noreply.github.com>
2025-11-02 16:48:28 +0700

    Create script for main.lua

---
+++ farm88/script for main.lua
@@ -0,0 +1,22 @@
+--Accept 60 75
+task.spawn(function()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/zcreatefilecc88.lua"))()
+end)
+
+--Accept money
+task.spawn(function()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/auto%20send%20money%20via%20trade.lua"))()
+end)
+
+--Up 8/8
+task.spawn(function()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/auto%20slot.lua"))()
+end)
+
+--Change acc
+loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/zcreatefilecc88.lua"))()
+
+
+
+
+
