c1d865a8e69e5d4443e2ea3d3d75ba997b1bb4c8
binhphuon <131878934+binhphuon@users.noreply.github.com>
2025-11-14 23:58:14 +0700

    Update rai tien.lua

--- farm88/rai tien.lua
+++ farm88/rai tien.lua
@@ -119,5 +119,9 @@ task.spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/evxncodes/mainroblox/main/anti-afk", true))()
 end)
 
+-- Create file
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/zcreatefilecc88.lua"))()
+end)
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/send%20small%20money.lua"))()
