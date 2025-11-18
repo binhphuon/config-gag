024a3250bfa5e28ed0771b6a24c9387f9321ca90
binhphuon <131878934+binhphuon@users.noreply.github.com>
2025-11-18 16:58:33 +0700

    Create gom 60 75.lua

---
+++ farm88/gom 60 75.lua
@@ -0,0 +1,5 @@
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/main/create%20file%20to%20change%20acc.lua"))()
+end)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/zHoangPhuc%20accept%20gift.lua"))()
