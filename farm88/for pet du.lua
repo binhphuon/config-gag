f78938dc5054fd65be9b8dd002842cf5f000432e
binhphuon <131878934+binhphuon@users.noreply.github.com>
2025-11-21 03:51:29 +0700

    Create for pet du.lua

---
+++ farm88/for pet du.lua
@@ -0,0 +1,11 @@
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/farm88/gom%2060%2075.lua"))()
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/main/auto%20sell%20pet.lua"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/zcreatefilecc88.lua"))()
+end)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/auto%20gift%20pet.lua"))()
