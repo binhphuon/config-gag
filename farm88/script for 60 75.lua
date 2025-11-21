cf36bca722e802127e913c0e801fe92a45f4d024
binhphuon <131878934+binhphuon@users.noreply.github.com>
2025-11-21 23:00:41 +0700

    Update script for 60 75.lua

--- farm88/script for 60 75.lua
+++ farm88/script for 60 75.lua
@@ -8,8 +8,4 @@ task.spawn(function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/zcreatefilecc88.lua"))()
 end)
 
-task.spawn(function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/clear%20workspace.lua"))()
-end)
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/zgift60feed88.lua"))() --luu y day la gift 60 only
