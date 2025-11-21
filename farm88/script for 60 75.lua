aac05d408433c961e77ecd7f875fa9bb0779d1e8
binhphuon <131878934+binhphuon@users.noreply.github.com>
2025-11-21 21:04:39 +0700

    Update script for 60 75.lua

--- farm88/script for 60 75.lua
+++ farm88/script for 60 75.lua
@@ -1,3 +1,5 @@
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/farm88/script%20for%2060%2075.lua"))()
+
 task.spawn(function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/unequip%20pet.lua"))()
 end)
