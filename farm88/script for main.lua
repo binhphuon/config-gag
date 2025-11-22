d3f0dd6efe15f43289565b8870c6586013cf2747
binhphuon <131878934+binhphuon@users.noreply.github.com>
2025-11-22 09:40:58 +0700

    Update script for main.lua

--- farm88/script for main.lua
+++ farm88/script for main.lua
@@ -1,30 +1,15 @@
---loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/farm88/script%20for%20main.lua"))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/farm88/for%20pet%20du.lua"))()
 
---Accept 60 75
 task.spawn(function()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Txlerz3636/Gag-Trading/main/Auto%20accept%20gift.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/main/auto%20sell%20pet.lua"))()
 end)
 
---Accept money
 task.spawn(function()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/auto%20send%20money%20via%20trade.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/zcreatefilecc88.lua"))()
 end)
 
---Up 8/8
 task.spawn(function()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/auto%20slot.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/unequip%20pet.lua"))()
 end)
 
---Unequip pet
-task.spawn(function()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/unequip%20pet.lua"))()
-end)
-
-
---Change acc
-loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/zcreatefile8885.lua"))()
-
-
-
-
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/auto%20gift%20pet.lua"))()
