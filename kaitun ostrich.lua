8cc6a84589350297718effc4160704c3209cdf04
binhphuon <131878934+binhphuon@users.noreply.github.com>
2025-12-10 01:39:10 +0700

    Update kaitun ostrich.lua

--- kaitun ostrich.lua
+++ kaitun ostrich.lua
@@ -396,6 +396,11 @@ task.spawn(function()
     end
 end)
 
+--Auto sell
+task.spawn(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/main/auto%20sell%20pet.lua"))()
+end
+	
 --Anti afk
 task.spawn(function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/auto%20change%20pet.lua", true))()
