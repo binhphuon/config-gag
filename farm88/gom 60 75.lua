5eecb978dc0e77e5954455f9b474741a07dab6df
binhphuon <131878934+binhphuon@users.noreply.github.com>
2025-12-10 01:48:50 +0700

    Update gom 60 75.lua

--- farm88/gom 60 75.lua
+++ farm88/gom 60 75.lua
@@ -4,4 +4,8 @@ task.spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/main/create%20file%20to%20change%20acc.lua"))()
 end)
 
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/main/auto%20sell%20pet.lua"))()
+end)
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/zHoangPhuc%20accept%20gift.lua"))()
