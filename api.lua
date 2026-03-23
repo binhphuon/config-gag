609a9bc6e5cb3478ab7db9436dd109bd23e66634
binhphuon <131878934+binhphuon@users.noreply.github.com>
2026-03-24 00:15:21 +0700

    Update api.lua

--- api.lua
+++ api.lua
@@ -1,2 +1,2 @@
-
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/api.lua"))()
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/hihiae/36var/refs/heads/main/asdasd"))()
