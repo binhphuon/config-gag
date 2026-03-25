458c7242b8036a59966787704d891da92f89b375
binhphuon <131878934+binhphuon@users.noreply.github.com>
2026-03-25 17:37:38 +0700

    Update kt mck.lua

--- kt mck.lua
+++ kt mck.lua
@@ -1,3 +1,4 @@
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/kt%20mck.lua"))()
 repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer
 getgenv().key = "PREMIUM-B8C34608"
 getgenv().Config = {
