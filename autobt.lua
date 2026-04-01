8f3bc6baca792e49909ba644fb08c6f2afa7ee1f
binhphuon <131878934+binhphuon@users.noreply.github.com>
2026-04-02 02:50:44 +0700

    Create autobt.lua

---
+++ autobt.lua
@@ -0,0 +1,25 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
+getgenv().tmconfig = {
+    key = "",
+    team = "Pirates", -- "Pirates" "Marines"
+    hpTimeout = 15,
+    targetTimeout = 10,
+    lowHealth = 4000,
+    safeHealth = 4500,
+    code = "",
+    useSkill = false, -- only x f gas fruit
+    equipPaleScarf = false,
+    webhookurl = "",
+    webhookEnable = false,
+    webhookSendMinutes = 5,
+    attackSpeed = 0.0001,
+    mode = 2,
+    fat = 10, -- 1 = normal UI, 10 = minimal UI 
+    sea = 3,
+    region = "Singapore",
+    trans = false, -- auto press V t rex 
+    bltween = true, 
+    speedcf = 0.000000001, 
+    bpsit = true, 
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Kuro2112/sorip/refs/heads/main/supanika-obfuscated.lua"))()
