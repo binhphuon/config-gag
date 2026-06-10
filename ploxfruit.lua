507002c83538c002384057ec104c6eb9a70d0065
binhphuon <131878934+binhphuon@users.noreply.github.com>
2026-06-10 12:57:52 +0700

    Update ploxfruit.lua

--- ploxfruit.lua
+++ ploxfruit.lua
@@ -19,4 +19,14 @@ getgenv().NAH = {
         EatFruit = false
     },
 }
+task.spawn(function()
+    local ok, err = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/binhphuon/config-gag/refs/heads/main/hope%20bf.lua"))()
+    end)
+    if not ok then
+        warn("[Load] Loi khi chay hope bf.lua: " .. tostring(err))
+    end
+end)
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/094bcde19464e77064beb3eb705d047d.lua"))()
+
+
